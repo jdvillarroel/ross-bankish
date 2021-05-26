57183db0e3cf662781d63cfafd752a049cf02038
Jesus Villarroel <jdvillarroel@gmail.com>
2021-05-26 17:57:48 -0500

    Regsiter form added. No functionality yet. Show/hide password feature added.

---
+++ src/components/Register.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="backdrop">
+    <div class="modal">
+      <form>
+        <div class="form-row">
+          <label for="fName">Nombre</label>
+          <input type="text" id="fName" required v-model="fName">
+        </div>
+        
+        <div class="form-row">
+          <label for="lName">Apellido</label>
+          <input type="text" id="lName" required v-model="lName">
+        </div>
+        
+        <div class="form-row">
+          <label for="email">Email</label>
+          <input type="email" id="email" required v-model="email">
+        </div>
+        
+        <div class="form-row">
+          <label for="password">Password</label>
+          <input :type="passType" id="password" required v-model="password" >
+          <p class="form-message" @click="togglePasswordType">Show/hide password</p>
+        </div>
+        
+        <div class="btn-container">
+          <button type="reset">Reset</button>
+          <button type="submit">Registrarse</button>
+        </div>
+        
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    // *********** Data ****************//
+    let isPassword = ref(true)
+    let fName = ref("")
+    let lName = ref("")
+    let email = ref("")
+    let password = ref("")
+    let passType = ref("password")
+
+    // *********** Methods **************** //
+    const togglePasswordType = () => {
+      isPassword.value = !isPassword.value
+
+      passType.value = isPassword.value ? "password" : "text"
+      console.log(passType.value)
+    }
+
+    return { isPassword, togglePasswordType, fName, lName, email, password, passType }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,21 @@
   <div class="home">
     <div class="container">
       <h1>Bienvenid@</h1>
+      <Register />
     </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Register from "../components/Register.vue"
 
 export default {
-  name: 'Home'
+  name: 'Home',
+
+  components: {
+    Register
+  }
 }
 </script>
 
@@ -19,5 +25,9 @@ export default {
     width: 90%;
     margin: 1rem auto;
     text-align: center;
+  }
+
+  .container h1 {
+    margin-bottom: 1rem;
   }
 </style>
